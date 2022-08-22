e058157928773f5428e665ea6d899ed49884153b
Lucas <lucaschmain@gmail.com>
2022-08-22 17:17:17 -0300

    [GROUP 14] Completing requirement 21 and 22

--- src/components/FilterDrinkButtons.js
+++ src/components/FilterDrinkButtons.js
@@ -2,7 +2,19 @@ import React, { useContext } from 'react';
 import MyRecipesContext from '../context/recipesContext/MyRecipesContext';
 
 function FilterDrinkButtons() {
-  const { drinksFilters } = useContext(MyRecipesContext);
+  const { drinksFilters, drinkFiltered, setDrinkFiltered } = useContext(MyRecipesContext);
+
+  const selectFilter = async ({ target }) => {
+    const { id } = target;
+    const END_POINT = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${id}`;
+    const TWELVE = 12;
+    const { drinks } = await fetch(END_POINT).then((response) => response.json());
+    setDrinkFiltered({
+      ...drinkFiltered,
+      drinkList: drinks.filter((_drink, index) => index < TWELVE),
+      toggle: !drinkFiltered.toggle,
+    });
+  };
 
   return (
     <div className="filters-buttons">
@@ -12,7 +24,8 @@ function FilterDrinkButtons() {
             key={ strCategory }
             type="button"
             data-testid={ `${strCategory}-category-filter` }
-            onClick={ () => {} }
+            id={ strCategory }
+            onClick={ selectFilter }
           >
             { strCategory }
           </button>
