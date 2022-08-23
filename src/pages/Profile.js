52599330cad93175135101962447f03292c5e16a
Ranieri Fernandes <ranierirfm@hotmail.com>
2022-08-23 15:44:16 -0300

    requisitos 57 e 59

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,10 +6,41 @@ import Header from '../components/Header';
 function Profile(props) {
   const { history } = props;
 
+  // const email = localStorage.getItem('user');
+  // const emailParse = JSON.parse(email);
   return (
     <>
       <Header history={ history } />
       <p>Página de Perfil</p>
+      <h1 data-testid="profile-email">email</h1>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        id="Done Recipes"
+      >
+        Done Recipes
+
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        id="Favorite Recipes"
+      >
+        Favorite Recipes
+
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        id="Logout"
+      >
+        Logout
+
+      </button>
+
       <Footer />
     </>
   );
