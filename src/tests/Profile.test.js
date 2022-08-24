f80cbad89b024d1e7bee6774916b43629c9f0048
Ranieri Fernandes <ranierirfm@hotmail.com>
2022-08-24 17:44:24 -0300

    requisito 62

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -2,11 +2,12 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
+
 import renderWithRouter from './helpers/renderWithRouter';
 
-test('Se a página Profile está presente e funcional', () => {
+describe('Testando a página Profile', () => {
+  test('Se a página Profile está presente e funcional', () => {
     const { history } = renderWithRouter(<App />);
-
     history.push('/profile');
 
     const donekButton = screen.getByTestId('profile-done-btn');
@@ -17,79 +18,59 @@ test('Se a página Profile está presente e funcional', () => {
     expect(favoriteButton).toBeDefined();
     expect(logoutButton).toBeDefined();
 
-
- 
   })
 
   test('Se o botão Done Recipes  ao ser clicado direciona a pagina correta', () => {
     const { history } = renderWithRouter(<App />);
-
     history.push('/profile');
 
     const donekButton = screen.getByTestId('profile-done-btn');
-
     userEvent.click(donekButton);
+  
     expect(history.location.pathname).toBe('/done-recipes');   
-
 
   })
 
   test('Se o botão Favorite Recipes  ao ser clicado direciona a pagina correta', () => {
     const { history } = renderWithRouter(<App />);
-
     history.push('/profile');
 
     const favoriteButton = screen.getByTestId('profile-favorite-btn');
-
     userEvent.click(favoriteButton);
+
     expect(history.location.pathname).toBe('/favorite-recipes');   
-
-
+  
   })
 
   test('Se o botão Logout  ao ser clicado direciona a pagina Login', () => {
     const { history } = renderWithRouter(<App />);
-
     history.push('/profile');
 
     const logoutButton = screen.getByTestId('profile-logout-btn');
-
     userEvent.click(logoutButton);
+
     expect(history.location.pathname).toBe('/');   
 
-
   })
 
-  test('Se o email do usuário aparece na tela', () => {
+  test('Se o email do usuário aparece na tela', () => {  
     const { history } = renderWithRouter(<App />);
 
-    history.push('/profile');
+    const emailInput = screen.getByRole("textbox");
+    const passwordInput = screen.getByPlaceholderText("Digite sua senha");
+    
+    userEvent.type(emailInput, 'meu@email.com')
+    userEvent.type(passwordInput, '1234567')
+    
+    const submitButton = screen.getByRole("button", { name: /enter/i });
+    userEvent.click(submitButton)
 
-  const  email = screen.getByTestId('profile-email');
+    const profileButton = screen.getByRole("button", { name: /profile icon/i });
+    userEvent.click(profileButton)
 
+    const email = screen.getByRole("heading", { name: /meu@email\.com/i });
 
-    expect(email).toBeDefined(); 
+    expect(email).toBeInTheDocument();
 
-
-  })
-
-
-//   test('Se o email do usuário aparece na tela', () => {  
-
-//     const { history } = renderWithRouter(<App />);
-
-//     const emailInput = screen.getByRole('textbox'); 
-//     const passwordInput = screen.getByPlaceholderText(/digite sua senha/i);
-//     const submitButton = screen.getByRole('button', { name: /enter/i });
-
-//     fireEvent.change(emailInput, {target: {value: 'ranierirfm@hotmail.com'}})
-//     fireEvent.change(passwordInput, {target: {value: '12345678'}})
-//     fireEvent.click(submitButton)
-
-//     history.push('/profile');
-
-//     const  email = screen.getByTestId('profile-email');
-
-//     expect(email).toEqual('ranierirfm@hotmail.com');  
-
-// } )
+})
+});
