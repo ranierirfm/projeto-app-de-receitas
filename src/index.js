0c68ab431a6ece08688d77586f2f6fc1e2664dda
Lucas <lucaschmain@gmail.com>
2022-08-24 15:46:20 -0300

    [GROUP 14] Completing Requirement 28

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
