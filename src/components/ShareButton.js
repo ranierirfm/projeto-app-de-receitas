2249e3257d6bfc7e4703587dfc4004dc10192a0c
Lucas <lucaschmain@gmail.com>
2022-08-25 15:57:27 -0300

    [GROUP 14] Completing Requirement 33

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import clipboardCopy from 'clipboard-copy';
 
-function ShareButton() {
+function ShareButton(props) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyUrl = () => {
+    const { url } = props;
+    clipboardCopy(`http://localhost:3000${url}`);
+    console.log(`http://localhost:3000${url}`);
+    setIsCopied(true);
+  };
+
   return (
-    <button
-      type="button"
-      data-testid="share-btn"
-    >
-      Compartilhar
-    </button>
+    <>
+      <button
+        type="button"
+        data-testid="share-btn"
+        onClick={ copyUrl }
+      >
+        Compartilhar
+      </button>
+      { isCopied && <p>Link copied!</p> }
+    </>
+
   );
 }
+
+ShareButton.propTypes = {
+  match: PropTypes.shape({
+    url: PropTypes.string.isRequired }),
+}.isRequired;
 
 export default ShareButton;
