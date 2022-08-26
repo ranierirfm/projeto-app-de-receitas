f6c47562817e540e80f1f1d0674f4c04a9311fbe
Ranieri Fernandes <ranierirfm@hotmail.com>
2022-08-26 15:14:04 -0300

    requisito 53

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,5 +1,6 @@
 import PropTypes, { object } from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
@@ -7,22 +8,32 @@ import shareIcon from '../images/shareIcon.svg';
 function FavoriteRecipes(props) {
   const { history } = props;
 
+  const [transfArea, settransfArea] = useState(false);
+
   const favRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+  function copiedLink() {
+    if (transfArea === true) {
+      return ' Link copied!';
+    }
+  }
 
   function filterRecipes() {
     if (favRecipes !== null || favRecipes.length !== 0) {
       return (
         favRecipes.map((
-          { id, nationality, category, image, name }, i,
+          { id, nationality, category, type, image, name }, i,
         ) => (
           <div key={ id }>
-            <img
-              data-testid={ `${i}-horizontal-image` }
-              src={ image }
-              alt={ name }
-              width="15px"
-              height="150px"
-            />
+            <Link to={ `/${type}s/${id}` }>
+              <img
+                data-testid={ `${i}-horizontal-image` }
+                src={ image }
+                alt={ name }
+                width="150px"
+                height="150px"
+              />
+            </Link>
             <div>
               <p
                 data-testid={ `${i}-horizontal-top-text` }
@@ -30,11 +41,13 @@ function FavoriteRecipes(props) {
                 {`${nationality} - ${category}`}
               </p>
               <br />
-              <p
-                data-testid={ `${i}-horizontal-name` }
-              >
-                { name }
-              </p>
+              <Link to={ `/${type}s/${id}` }>
+                <p
+                  data-testid={ `${i}-horizontal-name` }
+                >
+                  { name }
+                </p>
+              </Link>
             </div>
 
             <div>
@@ -42,15 +55,27 @@ function FavoriteRecipes(props) {
                 src={ shareIcon }
                 type="button"
                 data-testid={ `${i}-horizontal-share-btn` }
+                onClick={ () => {
+                  navigator.clipboard
+                    .writeText(`${window.location.origin}/${type}s/${id}`);
+                  settransfArea(true);
+                } }
               >
-                Compartilhar
+                <img
+                  src={ shareIcon }
+                  alt="Share Icon"
+                />
+                {copiedLink()}
               </button>
               <button
                 data-testid={ `${i}-horizontal-favorite-btn` }
                 type="button"
                 src={ blackHeartIcon }
               >
-                Desfavoritar
+                <img
+                  src={ blackHeartIcon }
+                  alt="Favorite Icon"
+                />
               </button>
             </div>
           </div>
